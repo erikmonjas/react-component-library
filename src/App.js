6942ca77f9daeb109dabcc48a1869d0747f47d4d
Erik Monjas <emonjas@gfi.es>
2019-06-24 15:55:47 +0200

    fix: cross field validation removed

--- src/App.js
+++ src/App.js
@@ -1,163 +1,157 @@
-import React from 'react';
-import Tabs from './components/Tabs';
-import Tab from './components/Tabs/Tab';
-import Input from './components/Form/Input/Input';
-import Select from './components/Form/Select/Select';
-import Checkbox from './components/Form/Checkbox/Checkbox';
-import Form from './components/Form';
-import RadioGroup from './components/Form/Radio/RadioGroup';
-import { FormContext } from './hooks/formHook';
+import React from "react";
+import Tabs from "./components/Tabs";
+import Tab from "./components/Tabs/Tab";
+import Input from "./components/Form/Input/Input";
+import Select from "./components/Form/Select/Select";
+import Checkbox from "./components/Form/Checkbox/Checkbox";
+import Form from "./components/Form";
+import RadioGroup from "./components/Form/Radio/RadioGroup";
+import { FormContext } from "./hooks/formHook";
 
 const App = () => {
-  const submitAction = formValues => console.log('Valid', formValues);
+  const submitAction = formValues => console.log("Valid", formValues);
 
-  const invalidAction = invalids => console.log('Invalid', invalids);
+  const invalidAction = invalids => console.log("Invalid", invalids);
 
-  const otherTextValidationFunction = value => value.includes('hi');
+  const otherTextValidationFunction = value => value.includes("hi");
 
   const numberValidationFunction = value => parseFloat(value) > 10.5;
 
   const selectOptions = [
-    { text: 'Option 0', value: '0' },
-    { text: 'Option 1', value: '1' },
-    { text: 'Option 2', value: '2' },
+    { text: "Option 0", value: "0" },
+    { text: "Option 1", value: "1" },
+    { text: "Option 2", value: "2" }
   ];
 
   const radioList = [
-    { text: 'Activate select', value: '0' },
-    { text: 'Activate checkbox', value: '1' },
-    { text: 'Disable both', value: '2' },
+    { text: "Option 0", value: "0" },
+    { text: "Option 1", value: "1" },
+    { text: "Option 2", value: "2" }
   ];
 
   return (
-    <div className='m-10 m-md-40'>
-      <div className='tabs-demo'>
-        <h2 className='fz-24 mb-10 font-weight-bold'>Tabs</h2>
+    <div className="m-10 m-md-40">
+      <div className="tabs-demo">
+        <h2 className="fz-24 mb-10 font-weight-bold">Tabs</h2>
         <Tabs
-          className='mb-40'
-          tabList={[{ title: 'Tab 0' }, { title: 'Tab 1' }, { title: 'Tab 2' }]}>
-          <Tab className='mt-20' order={0}>
+          className="mb-40"
+          tabList={[{ title: "Tab 0" }, { title: "Tab 1" }, { title: "Tab 2" }]}
+        >
+          <Tab className="mt-20" order={0}>
             <p>I'm tab 0</p>
           </Tab>
-          <Tab className='mt-20' order={1}>
+          <Tab className="mt-20" order={1}>
             <p>I'm tab 1</p>
           </Tab>
-          <Tab className='mt-20' order={2}>
+          <Tab className="mt-20" order={2}>
             <p>I'm tab 2</p>
           </Tab>
         </Tabs>
       </div>
 
-      <div className='form-demo'>
-        <h2 className='fz-24 font-weight-bold mb-20'>Form</h2>
+      <div className="form-demo">
+        <h2 className="fz-24 font-weight-bold mb-20">Form</h2>
         <Form submitAction={submitAction} invalidAction={invalidAction}>
-          <div className='row'>
-            <div className='col-12 col-md-4'>
+          <div className="row">
+            <div className="col-12 col-md-4">
               <Input
-                name='text'
-                label='Text'
-                errorMessage='Error'
-                defaultValue={'Default value'}
-                minLength='5'
+                name="text"
+                label="Text"
+                errorMessage="Error"
+                defaultValue={"Default value"}
+                minLength="5"
                 disabled
               />
             </div>
-            <div className='col-12 col-md-4'>
+            <div className="col-12 col-md-4">
               <Input
-                name='otherText'
-                label='Other text'
-                minLength='4'
-                errorMessage='Error'
+                name="otherText"
+                label="Other text"
+                minLength="4"
+                errorMessage="Error"
                 validationFunction={otherTextValidationFunction}
               />
             </div>
-            <div className='col-12 col-md-4'>
+            <div className="col-12 col-md-4">
               <Input
-                name='requiredText'
-                label='Required text'
+                name="requiredText"
+                label="Required text"
                 required
-                minLength='4'
-                errorMessage='Error'
+                minLength="4"
+                errorMessage="Error"
                 validationFunction={otherTextValidationFunction}
               />
             </div>
           </div>
-          <div className='row'>
-            <div className='col-12 col-md-4'>
+          <div className="row">
+            <div className="col-12 col-md-4">
               <Input
                 required
-                name='number'
-                label='Number'
-                errorMessage='Error'
-                type='number'
+                name="number"
+                label="Number"
+                errorMessage="Error"
+                type="number"
                 validationFunction={numberValidationFunction}
               />
             </div>
-            <div className='col-12 col-md-4'>
+            <div className="col-12 col-md-4">
               <Select
-                label='Initial text'
+                label="Initial text"
                 options={selectOptions}
-                name='select'
-                errorMessage='Error select'
+                name="select"
+                errorMessage="Error select"
+              />
+            </div>
+            <div className="col-12 col-md-4">
+              <Select
+                label="Initial text"
+                options={selectOptions}
+                name="disabledSelect"
+                errorMessage="Error select"
+                defaultValue="1"
+                disabled
               />
             </div>
           </div>
-          <div className='row'>
-            <div className='col-12 mt-20 mb-5'>
-              <RadioGroup radioList={radioList} name='radioGroup' defaultRadio='2' />
-            </div>
-          </div>
-          <div className='row d-flex align-items-end'>
-            <FormContext.Consumer>
-              {({ formValues }) => (
-                <>
-                  <div className='col-12 col-md-4 mt-15'>
-                    <Select
-                      label='Initial text'
-                      options={selectOptions}
-                      name='disabledSelect'
-                      errorMessage='Error select'
-                      defaultValue='1'
-                      disabled={!!formValues.radioGroup && formValues.radioGroup.value !== '0'}
-                    />
-                  </div>
-                  <div className='col-12 col-md-4 mt-15'>
-                    <Checkbox
-                      name='checkbox'
-                      label='Checkbox text'
-                      defaultChecked
-                      disabled={!!formValues.radioGroup && formValues.radioGroup.value !== '1'}
-                    />
-                  </div>
-                </>
-              )}
-            </FormContext.Consumer>
-          </div>
 
-          <div className='row'>
-            <div className='col-12 mt-15'>
+          <div className="row mb-15">
+            <div className="col-4">
+              <RadioGroup
+                radioList={radioList}
+                name="radioGroup"
+                defaultRadio="2"
+              />
+            </div>
+            <div className="col-4">
               <Checkbox
-                name='otherCheckbox'
-                label='Checkbox text'
-                errorMessage='This checkbox is required'
+                name="otherCheckbox"
+                label="Checkbox text"
+                errorMessage="This checkbox is required"
                 required
               />
             </div>
           </div>
-          <div className='d-flex justify-content-end'>
+
+          <div className="d-flex justify-content-end">
             <FormContext.Consumer>
               {value => (
                 <>
-                  <button className='button' onClick={() => console.log(value.formValues)}>
+                  <button
+                    className="button"
+                    onClick={() => console.log(value.formValues)}
+                  >
                     Log values
                   </button>
-                  <button className='button ml-10' onClick={() => console.log(value.invalids)}>
+                  <button
+                    className="button ml-10"
+                    onClick={() => console.log(value.invalids)}
+                  >
                     Log invalids
                   </button>
                 </>
               )}
             </FormContext.Consumer>
-            <button type='submit' className='button ml-10' formNoValidate>
+            <button type="submit" className="button ml-10" formNoValidate>
               Submit
             </button>
           </div>
