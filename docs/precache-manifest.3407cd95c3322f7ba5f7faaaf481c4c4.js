636dd8cf3cd876ec8fe8d1dbd06e00691da747a7
Erik Monjas <erik.monjas@gmail.com>
2019-07-21 20:16:49 +0200

    deploy

---
+++ docs/precache-manifest.3407cd95c3322f7ba5f7faaaf481c4c4.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7d268ed773677900cefa8bb5fc3bcb76",
+    "url": "/react-component-library/index.html"
+  },
+  {
+    "revision": "05dc9440fb0efdfbb772",
+    "url": "/react-component-library/static/css/main.7927168c.chunk.css"
+  },
+  {
+    "revision": "173d71fd2cf356c419fb",
+    "url": "/react-component-library/static/js/2.4cf0ac96.chunk.js"
+  },
+  {
+    "revision": "05dc9440fb0efdfbb772",
+    "url": "/react-component-library/static/js/main.b911c01f.chunk.js"
+  },
+  {
+    "revision": "fd5ebdb6784a12b52f1b",
+    "url": "/react-component-library/static/js/runtime~main.aa3fa71d.js"
+  },
+  {
+    "revision": "ee7cd8ed2dcec943251eb2763684fc6f",
+    "url": "/react-component-library/static/media/logo.ee7cd8ed.svg"
+  }
+]);
--- src/routes/index.js
+++ src/routes/index.js
@@ -1,9 +1,9 @@
-import React, { useState, useEffect } from "react";
-import { Route, Switch } from "react-router-dom";
-import FormContainer from "../containers/FormContainer";
-import TabsContainer from "../containers/TabsContainer";
-import HomeContainer from "../containers/HomeContainer";
-import Logo from "../logo.svg";
+import React, { useState, useEffect } from 'react';
+import { Route, Switch } from 'react-router-dom';
+import FormContainer from '../containers/FormContainer';
+import TabsContainer from '../containers/TabsContainer';
+import HomeContainer from '../containers/HomeContainer';
+import Logo from '../logo.svg';
 
 const Routes = () => {
   const [width, setWidth] = useState(window.innerWidth);
@@ -13,7 +13,7 @@ const Routes = () => {
   };
 
   useEffect(() => {
-    window.addEventListener("resize", () => {
+    window.addEventListener('resize', () => {
       getWidth();
     });
   }, []);
@@ -22,23 +22,20 @@ const Routes = () => {
     <>
       {width > 768 ? (
         <Switch>
-          <Route path="/" exact component={HomeContainer} />
-          <Route path="/form" exact component={FormContainer} />
-          <Route path="/tabs" exact component={TabsContainer} />
+          <Route path='/' exact component={HomeContainer} />
+          <Route component={HomeContainer} />
+          <Route path='/form' exact component={FormContainer} />
+          <Route path='/tabs' exact component={TabsContainer} />
         </Switch>
       ) : (
-        <div className="under-width">
-          <div className="under-width__inner">
-            <h2 className="font-weight-bold fz-24 text-center mb-10">
-              Component Library
-            </h2>
-            <img src={Logo} alt="logo" className="home-logo" />
-            <p className="mb-20 mt-10">
-              Your current screen size is {window.innerWidth}px
-            </p>
+        <div className='under-width'>
+          <div className='under-width__inner'>
+            <h2 className='font-weight-bold fz-24 text-center mb-10'>Component Library</h2>
+            <img src={Logo} alt='logo' className='home-logo' />
+            <p className='mb-20 mt-10'>Your current screen size is {window.innerWidth}px</p>
             <p>
-              Screen size must be over 768px to properly view this page. Try
-              rotating your device or accessing the page from another one.
+              Screen size must be over 768px to properly view this page. Try rotating your device or
+              accessing the page from another one.
             </p>
           </div>
         </div>
