b3fe744b0633bbe756a038f9ae37bb890a7319c7
Erik <erik.monjas@gmail.com>
2019-10-09 21:12:26 +0200

    deploy

---
+++ docs/precache-manifest.a7b6165fe5f00058fed0305c49e01d5c.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8fc1764bfbc8435f99a8182421d917a1",
+    "url": "/index.html"
+  },
+  {
+    "revision": "1671192cedf600184deb",
+    "url": "/static/css/main.126c488a.chunk.css"
+  },
+  {
+    "revision": "3c14b61d24c072c1d668",
+    "url": "/static/js/2.ef803dbf.chunk.js"
+  },
+  {
+    "revision": "1671192cedf600184deb",
+    "url": "/static/js/main.365393df.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "ee7cd8ed2dcec943251eb2763684fc6f",
+    "url": "/static/media/logo.ee7cd8ed.svg"
+  }
+]);
